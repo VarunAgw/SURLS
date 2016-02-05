1a27ba578ce6173f5c3916503baef0f670b9b3d4
Varun Agrawal <Varun@VarunAgw.com>
2016-02-06 03:38:28 +0530

    Updated surls_function to demo new structure

--- surls_functions.php
+++ surls_functions.php
@@ -1,17 +1,9 @@
 <?php
 
-return array(
-    'test1' => function () {
-        echo '<style>a{text-decoration: none;}</style>';
-        echo '<h1>';
-        echo '<marquee>';
-        echo '<a href="https://en.wikipedia.org/wiki/Bill_Gates">Bill Gates</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-        echo '<a href="http://ben10.wikia.com/wiki/Vilgax">Vilgax</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-        echo '</marquee>';
-        echo '</h1>';
-    },
-    'test2' => function () {
-        header('Location: http://www.boogle.com', true, 302);
-    },
-);
-?>
+function surls_handler_hello() {
+    echo 'Hello World!';
+}
+
+function surls_handler_boogle() {
+    header('Location: http://www.boogle.com', true, 302);
+}
