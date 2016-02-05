2b2de9c511487d1282e5d51e8786e18cccc35f85
Varun Agrawal <Varun@VarunAgw.com>
2016-02-06 04:13:44 +0530

    Improved examples

--- surls_functions.php
+++ surls_functions.php
@@ -1,9 +1,11 @@
 <?php
 
+// This will run on example.com/hello
 function surls_handler_hello() {
     echo 'Hello World!';
 }
 
-function surls_handler_boogle() {
+// This will run on example.com/google
+function surls_handler_google() {
     header('Location: http://www.boogle.com', true, 302);
 }
